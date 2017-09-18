5f00dd2c6f04f460f8acf02be56d5a57c901b434
Mohammad Habibi <habibi.mh@gmail.com>
2017-09-18 18:43:08 +0430

    Fix migration

--- console/migrations/m170918_065053_shop.php
+++ console/migrations/m170918_065053_shop.php
@@ -13,39 +13,40 @@ class m170918_065053_shop extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%category}}', [
+        $this->createTable('{{%catalog_category}}', [
             'id' => Schema::TYPE_PK,
             'parent_id' => Schema::TYPE_INTEGER,
             'title' => Schema::TYPE_STRING,
             'desc' => Schema::TYPE_STRING,
         ], $tableOptions);
-        $this->addForeignKey('fk-category-parent_id-category-id', '{{%category}}', 'parent_id', '{{%category}}', 'id', 'CASCADE');
-        $this->createTable('{{%product}}', [
+        $this->addForeignKey('fk-category-parent_id-category-id', '{{%catalog_category}}', 'parent_id', '{{%catalog_category}}', 'id', 'CASCADE');
+        $this->createTable('{{%catalog_product}}', [
             'id' => Schema::TYPE_PK,
             'title' => Schema::TYPE_STRING,
-            'category_id' => Schema::TYPE_INTEGER,
+            'model' => Schema::TYPE_STRING,
+            'catalog_category_id' => Schema::TYPE_INTEGER,
             'imgUrl' => Schema::TYPE_STRING,
             'desc' => Schema::TYPE_TEXT,
             'price' => Schema::TYPE_INTEGER,
             'stock' => Schema::TYPE_INTEGER,
             'status' => Schema::TYPE_SMALLINT,
-            'attributes' => 'LONGBLOB',
+            'attribute_values' => 'LONGBLOB',
         ], $tableOptions);
-        $this->addForeignKey('fk-product-category_id-category-id', '{{%product}}', 'category_id', '{{%category}}', 'id', 'CASCADE');
-        $this->createTable('{{%attribute}}', [
+        $this->addForeignKey('fk-product-category_id-category-id', '{{%catalog_product}}', 'catalog_category_id', '{{%catalog_category}}', 'id', 'CASCADE');
+        $this->createTable('{{%catalog_attribute}}', [
             'id' => Schema::TYPE_PK,
-            'category_id' => Schema::TYPE_INTEGER,
+            'catalog_category_id' => Schema::TYPE_INTEGER,
             'title' => Schema::TYPE_STRING,
             'desc' => Schema::TYPE_STRING,
         ], $tableOptions);
-        $this->addForeignKey('fk-attribute-category_id-category-id', '{{%attribute}}', 'category_id', '{{%category}}', 'id', 'CASCADE');
-        $this->createTable('{{%attribute_option}}', [
+        $this->addForeignKey('fk-attribute-category_id-category-id', '{{%catalog_attribute}}', 'catalog_category_id', '{{%catalog_category}}', 'id', 'CASCADE');
+        $this->createTable('{{%catalog_attribute_option}}', [
             'id' => Schema::TYPE_PK,
-            'attribute_id' => Schema::TYPE_INTEGER,
+            'catalog_attribute_id' => Schema::TYPE_INTEGER,
             'title' => Schema::TYPE_STRING,
         ], $tableOptions);
-        $this->addForeignKey('fk-attribute_option-attribute_id-attribute-id', '{{%attribute_option}}', 'attribute_id', '{{%attribute}}', 'id', 'CASCADE');
-        $this->createTable('{{%order}}', [
+        $this->addForeignKey('fk-attribute_option-attribute_id-attribute-id', '{{%catalog_attribute_option}}', 'catalog_attribute_id', '{{%catalog_attribute}}', 'id', 'CASCADE');
+        $this->createTable('{{%catalog_order}}', [
             'id' => Schema::TYPE_PK,
             'customer_id' => Schema::TYPE_INTEGER,
             'created_at' => Schema::TYPE_INTEGER,
@@ -56,26 +57,26 @@ class m170918_065053_shop extends Migration
             'notes' => Schema::TYPE_TEXT,
             'status' => Schema::TYPE_STRING,
         ], $tableOptions);
-        $this->addForeignKey('fk-order-customer_id-user-id', '{{%order}}', 'customer_id', '{{%user}}', 'id', 'CASCADE');
-        $this->createTable('{{%order_item}}', [
+        $this->addForeignKey('fk-order-customer_id-user-id', '{{%catalog_order}}', 'customer_id', '{{%user}}', 'id', 'CASCADE');
+        $this->createTable('{{%catalog_order_item}}', [
             'id' => Schema::TYPE_PK,
-            'order_id' => Schema::TYPE_INTEGER,
+            'catalog_order_id' => Schema::TYPE_INTEGER,
             'title' => Schema::TYPE_STRING,
             'price' => Schema::TYPE_MONEY,
-            'product_id' => Schema::TYPE_INTEGER,
+            'catalog_product_id' => Schema::TYPE_INTEGER,
             'quantity' => Schema::TYPE_FLOAT,
         ], $tableOptions);
-        $this->addForeignKey('fk-order_item-order_id-order-id', '{{%order_item}}', 'order_id', '{{%order}}', 'id', 'CASCADE');
-        $this->addForeignKey('fk-order_item-product_id-product-id', '{{%order_item}}', 'product_id', '{{%product}}', 'id', 'SET NULL');
+        $this->addForeignKey('fk-order_item-order_id-order-id', '{{%catalog_order_item}}', 'catalog_order_id', '{{%catalog_order}}', 'id', 'CASCADE');
+        $this->addForeignKey('fk-order_item-product_id-product-id', '{{%catalog_order_item}}', 'catalog_product_id', '{{%catalog_product}}', 'id', 'SET NULL');
     }
 
     public function down()
     {
-        $this->dropTable('{{%order_item}}');
-        $this->dropTable('{{%order}}');
-        $this->dropTable('{{%attribute_option}}');
-        $this->dropTable('{{%attribute}}');
-        $this->dropTable('{{%product}}');
-        $this->dropTable('{{%category}}');
+        $this->dropTable('{{%catalog_order_item}}');
+        $this->dropTable('{{%catalog_order}}');
+        $this->dropTable('{{%catalog_attribute_option}}');
+        $this->dropTable('{{%catalog_attribute}}');
+        $this->dropTable('{{%catalog_product}}');
+        $this->dropTable('{{%catalog_category}}');
     }
 }
